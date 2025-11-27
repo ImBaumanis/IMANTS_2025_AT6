638ca3ee4200b47041d93e6a7c06339d7046ca2b
IsmaelNr8 <baumanis.imants@inbox.lv>
2025-11-27 11:05:15 +0100

    Commit 2

---
+++ code_with_instances.py
@@ -0,0 +1,11 @@
+class UserInputValidator:
+    def validate_positive_integers(list):
+        final_list = []
+        for string in list:
+            if string.isdigit():
+                final_list.append(string)
+
+        return print(f"The list contains {len(final_list)} integer(s) ")
+
+c1 = UserInputValidator
+c2 = UserInputValidator
