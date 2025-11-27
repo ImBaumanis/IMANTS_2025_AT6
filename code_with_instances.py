39aad6c08ac41436ab26b1d1e9993b49656e56ce
IsmaelNr8 <baumanis.imants@inbox.lv>
2025-11-27 11:15:10 +0100

    Commit 3

--- code_with_instances.py
+++ code_with_instances.py
@@ -9,3 +9,7 @@ class UserInputValidator:
 
 c1 = UserInputValidator
 c2 = UserInputValidator
+
+list_of_strings = ["sun", "1973", "-3", "weather", "3.14", "24", "200"]
+c1.validate_positive_integers(list_of_strings)
+c2.validate_positive_integers(list_of_strings)
